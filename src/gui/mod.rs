66d398e86803711e25bc96533c20cef3e4b0b2ce
Norman Ritchie <born2lose@protonmail.com>
2017-10-05 14:25:03 +0300

    Small change

--- src/gui/mod.rs
+++ src/gui/mod.rs
@@ -3,6 +3,7 @@ use gtk::prelude::*;
 use gtk::{Builder, Button, Window};
 
 use core::dialog;
+use core::settings;
 
 pub fn launch_gtk() {
     if gtk::init().is_err() {
@@ -17,11 +18,11 @@ pub fn launch_gtk() {
      */
     let ui = include_str!("../resources/glade/app.ui");
     let builder = Builder::new_from_string(ui);
-    let window: Window = builder.get_object("main_window").expect(
+    let main_window: Window = builder.get_object("main_window").expect(
         "Couldn't get main_window",
     );
 
-    window.connect_delete_event(|_, _| {
+    main_window.connect_delete_event(|_, _| {
         gtk::main_quit();
         Inhibit(false)
     });
@@ -29,6 +30,8 @@ pub fn launch_gtk() {
 
     let open_button = dialog::click_open;
     let about_button = dialog::click_about;
+    let settings_button = settings::create_setting_window;
+
     /*
      *   Here we define `about` variable which implement
      *   "connect_clicked" method with `about_button` variable.
@@ -37,12 +40,13 @@ pub fn launch_gtk() {
         "Couldn't get about_button",
     );
 
-    let _setting: Button = builder.get_object("setup_btn").expect(
+    let setting: Button = builder.get_object("setup_btn").expect(
         "Couldn't get setup_button",
     );
 
 
-    open_button(&builder, &window);
+    open_button(&builder, &main_window);
+    setting.connect_clicked(move |_| settings_button());
     // about_button and about = супер костыль.
     about.connect_clicked(move |x| about_button(x, &builder));
 
@@ -50,6 +54,6 @@ pub fn launch_gtk() {
      *   Recursively shows a widget (any child widgets)
      *   and start the gtk main loop.
      */
-    window.show_all();
+    main_window.show_all();
     gtk::main();
 }
